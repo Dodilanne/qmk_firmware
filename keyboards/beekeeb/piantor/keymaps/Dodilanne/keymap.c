cd0dc5ac78de11bf8a860ae034e9c3c3f76cf6dd
Vincent Audet <vincaudet@gmail.com>
2023-11-04 10:38:36 -0400

    Fix build

--- keyboards/beekeeb/piantor/keymaps/Dodilanne/keymap.c
+++ keyboards/beekeeb/piantor/keymaps/Dodilanne/keymap.c
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS, KC_COMM,    KC_4,    KC_5,    KC_6,    KC_PERC,                          KC_HOME,    KC_PAGE_DOWN,    KC_PAGE_UP,    KC_END, KC_TRNS,KC_TRNS,
         KC_TRNS, KC_DOT,    KC_1,    KC_2,    KC_3,    KC_0,                          KC_UP,    KC_LEFT, KC_KB_VOLUME_UP,  G(S(KC_LBRC)), KC_TRNS,KC_TRNS,
                                             KC_TRNS, KC_TRNS, KC_TRNS,       CW_TOGG,  KC_TRNS,  KC_TRNS
-    )
+    ),
 
     // Layer 5: Media
     [5] = LAYOUT_split_3x6_3(
@@ -51,5 +51,5 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS, KC_TRNS,    KC_TRNS,    LSG(KC_4),    KC_MEDIA_PLAY_PAUSE,    KC_TRNS,                               KC_MEDIA_PREV_TRACK,    KC_KB_VOLUME_DOWN,    KC_KB_VOLUME_UP,    KC_MEDIA_NEXT_TRACK,    KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,                               KC_TRNS,    KC_AUDIO_MUTE,    KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,
                                             KC_TRNS, KC_TRNS, KC_TRNS,               KC_TRNS,  KC_TRNS,  KC_TRNS
-    ),
+    )
 };
