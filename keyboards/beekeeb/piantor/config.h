fd808786ceee8e3272812c69de76684582b91bcd
Dodilanne <dodilanne@ada-anvil.io>
2024-02-09 09:03:47 -0500

    Home row mods

--- keyboards/beekeeb/piantor/config.h
+++ keyboards/beekeeb/piantor/config.h
@@ -3,7 +3,13 @@
 
 #pragma once
 
-#define TAPPING_TERM 325
+/* #define TAPPING_TERM 325 */
+
+// Configure the global tapping term (default: 200ms)
+#define TAPPING_TERM 200
+
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+#define QUICK_TAP_TERM 0
 
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 1000U
